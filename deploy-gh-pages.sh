a2ef8cae3a0caf78ba03047d675bca0b9a24af80
Acconut <maerious@gmail.com>
2015-07-20 23:15:41 +0200

    Pass deploy if javadoc did not change

--- deploy-gh-pages.sh
+++ deploy-gh-pages.sh
@@ -3,6 +3,6 @@
 git checkout gh-pages
 javadoc io.tus.java.client -sourcepath ./src/main/java -d ./javadoc
 git add ./javadoc
-git commit -m "Update javadoc for $(git rev-parse HEAD)"
+git commit -m "Update javadoc for $(git rev-parse HEAD)" || true
 git push origin gh-pages
 git checkout master
