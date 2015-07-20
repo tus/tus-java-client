4c9bc71a21eec03dee36f2c5cc6ca36ee0f80d9e
Acconut <maerious@gmail.com>
2015-07-20 23:39:26 +0200

    Merge before generating javadoc

--- deploy-gh-pages.sh
+++ deploy-gh-pages.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 git checkout gh-pages
+git merge master
 javadoc io.tus.java.client -sourcepath ./src/main/java -d ./javadoc
 git add ./javadoc
 git commit -m "Update javadoc for $(git rev-parse HEAD)" || true
