ec6178d9ca3921f9b24a1af4f9bfb2747db9450a
Acconut <maerious@gmail.com>
2015-07-20 23:09:51 +0200

    Enable automatic publishing to GitHub Pages

---
+++ deploy-gh-pages.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+git checkout gh-pages
+javadoc io.tus.java.client -sourcepath ./src/main/java -d ./javadoc
+git add ./javadoc
+git commit -m "Update javadoc for $(git rev-parse HEAD)"
+git push origin gh-pages
+git checkout master
