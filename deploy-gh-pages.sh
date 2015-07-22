f1fb16b9216334454f2f50bf91a0a76480345a41
Acconut <maerious@gmail.com>
2015-07-22 11:47:39 +0200

    Switch away from private account from publishing docs

--- deploy-gh-pages.sh
+++ deploy-gh-pages.sh
@@ -2,14 +2,14 @@
 
 echo "Publishing javadocs to GitHub pages..."
 
-git clone --quiet --branch=gh-pages https://account:$GITHUB_TOKEN@github.com/tus/tus-java-client.git ../gh-pages
+git clone --quiet --branch=gh-pages https://tus-bot:$GITHUB_TOKEN@github.com/tus/tus-java-client.git ../gh-pages
 commit=$(git rev-parse HEAD)
 javadoc io.tus.java.client -sourcepath ./src/main/java -d ../gh-pages/javadoc
 
 cd ../gh-pages
 
-git config user.email "maerious@gmail.com"
-git config user.name "Travis CI in the name of Acconut"
+git config user.email "tim@transloadit.com"
+git config user.name "tus' bot from Travis CI"
 
 git add ./javadoc
 git commit -m "Update javadoc for ${commit}"
