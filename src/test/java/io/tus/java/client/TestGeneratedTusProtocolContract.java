194b752ebbf7334b663e13afbc37fa943faff4c3
Kevin van Zonneveld <kevin@vanzonneveld.net>
2026-05-26 21:22:43 +0200

    Fix generated contract lint

--- src/test/java/io/tus/java/client/TestGeneratedTusProtocolContract.java
+++ src/test/java/io/tus/java/client/TestGeneratedTusProtocolContract.java
@@ -4,7 +4,6 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
 
 /**
  * Tests the generated API2 protocol contract canary.
